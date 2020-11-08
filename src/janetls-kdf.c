69c0c5457b4d095c7c78b504e1619265802d7816
Levi Schuck <me@levischuck.com>
2020-11-08 15:24:29 -0600

    Add concat kdf

--- src/janetls-kdf.c
+++ src/janetls-kdf.c
@@ -23,14 +23,17 @@
 #include "janetls.h"
 #include "janetls-kdf.h"
 #include "janetls-md.h"
+#include "mbedtls/platform_util.h"
 
 static Janet hkdf(int32_t argc, Janet * argv);
 static Janet pbkdf2(int32_t argc, Janet * argv);
+static Janet concatkdf(int32_t argc, Janet * argv);
 
 static const JanetReg cfuns[] =
 {
   {"kdf/hkdf", hkdf, ""},
   {"kdf/pbkdf2", pbkdf2, ""},
+  {"kdf/concatkdf", concatkdf, ""},
   {NULL, NULL, NULL}
 };
 
@@ -62,7 +65,7 @@ static Janet hkdf(int32_t argc, Janet * argv)
         "bytes with %p, but %d bytes were requested.", max_length, argv[0], length);
     }
   }
-  
+
   if (argc > 3)
   {
     salt = janet_to_bytes(argv[3]);
@@ -78,9 +81,9 @@ static Janet hkdf(int32_t argc, Janet * argv)
   {
     janet_panic("Could not allocate memory");
   }
-  
+
   ret = mbedtls_hkdf(md_info, salt.bytes, salt.len, key.bytes, key.len, info.bytes, info.len, output, length);
-  
+
   if (ret == 0)
   {
     result = janet_wrap_string(janet_string(output, length));
@@ -144,5 +147,138 @@ static Janet pbkdf2(int32_t argc, Janet * argv)
   mbedtls_md_free(&md_ctx);
   janet_sfree(output);
   check_result(ret);
+  return result;
+}
+
+// A reference implementation
+// https://github.com/pyca/cryptography/blob/master/src/cryptography/hazmat/primitives/kdf/concatkdf.py
+
+static Janet concatkdf(int32_t argc, Janet * argv)
+{
+  janet_arity(argc, 2, 6);
+  mbedtls_md_type_t alg = symbol_to_alg(argv[0]);
+  JanetByteView key = janet_to_bytes(argv[1]);
+  const mbedtls_md_info_t * md_info = mbedtls_md_info_from_type(alg);
+  size_t md_length = mbedtls_md_get_size(md_info);
+  size_t length = md_length;
+  int hmac = 1;
+  mbedtls_md_context_t md_ctx;
+  JanetByteView salt = empty_byteview();
+  JanetByteView otherinfo = empty_byteview();
+  uint8_t zeros[MBEDTLS_MD_MAX_SIZE];
+  uint8_t working[MBEDTLS_MD_MAX_SIZE];
+  Janet result = janet_wrap_nil();
+  int ret = 0;
+  uint8_t * buf = NULL;
+  uint8_t * working_buf = NULL;
+  size_t length_remaining = 0;
+  uint32_t counter = 1;
+
+  if (argc > 2)
+  {
+    otherinfo = janet_to_bytes(argv[2]);
+  }
+
+  if (argc > 3)
+  {
+    salt = janet_to_bytes(argv[3]);
+  }
+
+  if (argc > 4)
+  {
+    length = janet_getinteger(argv, 4);
+  }
+
+  if (argc > 5)
+  {
+    hmac = janet_getboolean(argv, 5);
+  }
+
+  if (salt.len == 0 && hmac)
+  {
+    salt.bytes = zeros;
+    salt.len = md_length;
+    mbedtls_platform_zeroize(zeros, md_length);
+  }
+  else if (salt.len > 0 && !hmac)
+  {
+    janet_panic("A salt value can only be used with an hmac");
+  }
+
+  buf = janet_smalloc(length);
+  working_buf = buf;
+
+  if (buf == NULL)
+  {
+    janet_panic("Could not allocate memory");
+  }
+
+  length_remaining = length;
+  mbedtls_platform_zeroize(working, MBEDTLS_MD_MAX_SIZE);
+  mbedtls_md_init(&md_ctx);
+
+  retcheck(mbedtls_md_setup(&md_ctx, md_info, hmac));
+
+  if (hmac)
+  {
+    retcheck(mbedtls_md_hmac_starts(&md_ctx, salt.bytes, salt.len));
+  }
+
+
+  while (length_remaining > 0)
+  {
+    uint8_t big_endian_counter[4];
+    big_endian_counter[0] = counter & 0x000000ff;
+    big_endian_counter[1] = counter & 0x0000ff00;
+    big_endian_counter[2] = counter & 0x00ff0000;
+    big_endian_counter[3] = counter & 0xff000000;
+
+    if (hmac)
+    {
+      retcheck(mbedtls_md_hmac_reset(&md_ctx));
+      retcheck(mbedtls_md_hmac_update(&md_ctx, big_endian_counter, 4));
+      retcheck(mbedtls_md_hmac_update(&md_ctx, key.bytes, key.len));
+      if (otherinfo.len) {
+        retcheck(mbedtls_md_hmac_update(&md_ctx, otherinfo.bytes, otherinfo.len));
+      }
+      retcheck(mbedtls_md_hmac_finish(&md_ctx, working));
+    }
+    else
+    {
+      retcheck(mbedtls_md_starts(&md_ctx));
+      retcheck(mbedtls_md_update(&md_ctx, big_endian_counter, 4));
+      retcheck(mbedtls_md_update(&md_ctx, key.bytes, key.len));
+      if (otherinfo.len) {
+        retcheck(mbedtls_md_update(&md_ctx, otherinfo.bytes, otherinfo.len));
+      }
+      retcheck(mbedtls_md_finish(&md_ctx, working));
+    }
+
+    if (length_remaining >= md_length)
+    {
+      memcpy(working_buf, working, md_length);
+      length_remaining -= md_length;
+    }
+    else
+    {
+      memcpy(working_buf, working, length_remaining);
+      length_remaining = 0;
+    }
+
+    counter++;
+    working_buf += md_length;
+  }
+
+  if (ret == 0)
+  {
+    result = janet_wrap_string(janet_string(buf, length));
+  }
+
+end:
+  mbedtls_platform_zeroize(buf, length);
+  janet_sfree(buf);
+  mbedtls_md_free(&md_ctx);
+  check_result(ret);
+
   return result;
 }
--- test/kdf.janet
+++ test/kdf.janet
@@ -10,4 +10,15 @@
 # https://www.ietf.org/rfc/rfc6070.html
 (hex/encode (kdf/pbkdf2 :sha1 "password" "salt" 20 2))
 "ea6c014dc72d6f8ccd1ed92ace1d41f0d8de8957"
+
+# https://nvlpubs.nist.gov/nistpubs/Legacy/SP/nistspecialpublication800-56ar.pdf
+# Section 5.8.1 (page 46-48)
+# OR
+# https://nvlpubs.nist.gov/nistpubs/SpecialPublications/NIST.SP.800-56Ar3.pdf
+# Section 5.8.2.1 (page 55)
+# PLUS "one-step key-derivation"
+# https://nvlpubs.nist.gov/nistpubs/SpecialPublications/NIST.SP.800-56Cr1.pdf
+# Section 4.1 (page 11-14)
+(hex/encode (kdf/concatkdf :sha1 "hello" "e" "" 41 false))
+"cb822fecff9f3b64c740a9c05beb1357cb3b961eee43dbc3fdcb4dbf3ee10f78d46cd9e63ca485a2e1"
 (run-tests!)
